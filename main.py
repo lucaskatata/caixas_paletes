2b8fdad5e2e0702dd35775d2a80d3b92c49fb05d
Lucas Katata <lkbiondo@gmail.com>
2025-11-07 14:23:06 -0300

    subindo arquivos

---
+++ main.py
@@ -0,0 +1,26 @@
+# %%
+import pandas as pd
+import streamlit as st
+
+arquivo = r"D:\Lucas\GitHub\carreta\dados.xlsx"
+
+df = pd.read_excel(arquivo, sheet_name='tabela')
+
+
+df['SKU CAIXA'] = df['SKU CAIXA'].str.upper()
+
+st.set_page_config(layout='wide', page_icon='📦', page_title='MF - localização caixas')
+st.title('MF - Localização caixas paletes')
+
+sku = st.text_input('Pesquisar sku')
+
+if not sku:
+    st.stop()
+else:    
+    sku = (sku.replace(' ', '_') + '_lj').upper()
+    palete = df[df['SKU CAIXA'] == sku].iloc[0]['PALET (40 +-)']
+    posicao = df[df['SKU CAIXA'] == sku].iloc[0]['POSIÇÃO (1 A 12)']
+    st.metric(label='SKU', value=sku)   
+    st.metric(label='Palete', value=palete)
+    st.metric(label='Posição', value=posicao)
+
